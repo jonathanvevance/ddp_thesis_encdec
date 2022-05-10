6c2a5620d469833000b299ef1bde5ffe05a46e87
Jonathan Ve Vance <jonathanvevance@gmail.com>
2022-05-10 10:42:19 +0530

    plan

---
+++ src/train.py
@@ -0,0 +1,11 @@
+"""Python file to train the model."""
+
+"""
+
+MITUSPTO = rxn smiles
+LHS smiles      --> graph obj
+graph obj       --> (MPNN) node features
+node features   --> (Transformer) output sequence
+output sequence --> (loss)
+
+"""
